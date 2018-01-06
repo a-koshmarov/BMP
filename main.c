05ac49b1ae3e8d083d277ff8be9b5e9af4e55bcb
a-koshmarov <35034325+a-koshmarov@users.noreply.github.com>
2018-01-06 19:38:05 +0300

    Changing variable names

--- main.c
+++ main.c
@@ -9,8 +9,10 @@ void BmpToGrey(char *filename) {
     double avg;
     int padding = 0;
 
+// Открываем файл и проверяем его наличие
     FILE *infile;
     infile = fopen(filename, "rb");
+
     if (infile == NULL) {
         printf("No such file\n");
         return;
@@ -21,20 +23,20 @@ void BmpToGrey(char *filename) {
 // Считываем header файла
     fread(header, sizeof(unsigned char), 54, infile);
 
-// Берем размеры из header'a
+// Берем размеры картинки из header'a
     width = *(int *) &header[18];
     height = *(int *) &header[22];
 //    printf("%d\n", height);
 
 
-// Отсутп в конце каждой строки
+// Находим отсутп в конце каждой строки
     while ((width * 3 + padding) % 4 != 0) {
         padding++;
     }
 //    printf("%d\n", padding);
 
 // Длина строки с отступом
-    int widthnew = width * 3 + padding;
+    int width_pad = width * 3 + padding;
 
 // Записываем header в выходной файл
     FILE *outfile;
@@ -42,36 +44,35 @@ void BmpToGrey(char *filename) {
     fwrite(header, 1, 54, outfile);
 
 // Выделяем память на строку с отступом
-    unsigned char *data = (unsigned char *) malloc(widthnew * sizeof(unsigned int));
+    unsigned char *pixels = (unsigned char *) malloc(width_pad * sizeof(unsigned int));
 
 // Считываем файл построчно
     for (int i = 0; i < height; i++) {
-// Считываем всю строку с отступом в data
-        fread(data, sizeof(unsigned char), widthnew, infile);
+// Считываем всю строку с отступом в pixels
+        fread(pixels, sizeof(unsigned char), width_pad, infile);
 //        count = 0;
 
 // Считваем каждый пиксель
         for (int j = 0; j < width * 3; j += 3) {
 // Находим среднее цветовое значение пикселя
-            avg = round((data[j] + data[j + 1] + data[j + 2]) / 3.0);
+            avg = round((pixels[j] + pixels[j + 1] + pixels[j + 2]) / 3.0);
             unsigned char avgu = (unsigned char) avg;
 //            printf("(%d) ", avgu);
 // Присваиваем среднее значение всему пикселю
-            data[j] = avgu;
-            data[j + 1] = avgu;
-            data[j + 2] = avgu;
+            pixels[j] = avgu;
+            pixels[j + 1] = avgu;
+            pixels[j + 2] = avgu;
 
 //            count++;
         }
 //        printf(" %d \n\n", count);
 // Запиываем строку с отступом в выходной файл
-        fwrite(data, sizeof(unsigned char), widthnew, outfile);
+        fwrite(pixels, sizeof(unsigned char), width_pad, outfile);
     }
 //    printf("%d", count);
     fclose(infile);
     fclose(outfile);
-    free(data);
-
+    free(pixels);
 }
 
 int main() {
